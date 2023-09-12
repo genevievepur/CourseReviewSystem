0cd87e91883ec96f01346efca1de800f8472e182
Genevieve <upm6ce@virginia.edu>
2023-09-12 19:32:51 -0400

    Copied Course, Student, and Review classes

---
+++ src/main/java/edu/virginia/cs/coursereviewsystem/Course.java
@@ -0,0 +1,19 @@
+package edu.virginia.cs.coursereviewsystem;
+
+public class Course {
+    private String department;
+    private int catalogNumber;
+    public Course(String department, int catalogNumber) {
+        this.department = department;
+        this.catalogNumber = catalogNumber;
+    }
+
+    public String getDepartment() { return department; }
+
+    public int getCatalogNumber() { return catalogNumber; }
+
+    public String getCourseTitle() {
+        String title = this.department + " " + this.catalogNumber;
+        return title;
+    }
+}

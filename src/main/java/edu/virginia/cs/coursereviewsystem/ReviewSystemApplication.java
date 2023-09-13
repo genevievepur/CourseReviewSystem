95ec4dc4285073b717fbccfabec9487734d187da
Genevieve <upm6ce@virginia.edu>
2023-09-12 20:00:51 -0400

    copied in the contents for ReviewSystemApplication

--- src/main/java/edu/virginia/cs/coursereviewsystem/ReviewSystemApplication.java
+++ src/main/java/edu/virginia/cs/coursereviewsystem/ReviewSystemApplication.java
@@ -8,16 +8,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ReviewSystemApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ReviewSystemApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        launch();
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ReviewSystemApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("ReviewSystem");
+        stage.setScene(scene);
+        stage.show();
     }
 }
